37d6aa67ce96b7ff9bd50a39a9f0b42dcd664950
alex-94-k <al941994.94@gmail.com>
2024-12-09 16:55:01 +0100

    Modal form was done

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,3 +7,26 @@ burger.addEventListener('click', function(){
     burger.classList.toggle('navbar__burger-active');
     menu.classList.toggle('navbar__wrapper-active');
 });
+
+/* Modal */
+
+const btn = document.querySelector('.navbar__contacts');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close')
+
+btn.addEventListener('click', function(){
+    overlay.classList.add('overlay__active');
+    modal.classList.add('modal__active');
+});
+
+closeBtn.addEventListener('click', function(){
+    overlay.classList.remove('overlay__active');
+    modal.classList.remove('modal__active');
+
+    const errorMessages = document.querySelectorAll('.error__message');
+
+    errorMessages.forEach(message => {
+    message.style.display = 'none';
+  });
+});
